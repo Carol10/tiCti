a95040a0bebcd6fbd01c9710df3c8f3dbd4c36c8
AA12RTYUIOPL <Phablulo@MacBook-Pro-de-Phablulo.local>
2015-11-20 15:40:47 -0300

    asd
    dd

--- servidor/server_api_min.swift
+++ servidor/server_api_min.swift
@@ -291,7 +291,11 @@ class ticti: NSObject , NSStreamDelegate{
             
             NSURLSession.sharedSession().dataTaskWithRequest(request, completionHandler: { (data, response, error) -> Void in
                 let result = try! NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions(rawValue: 0))
+<<<<<<< Updated upstream
                 print(result)
+=======
+                
+>>>>>>> Stashed changes
                 if(result["status"] as! String == "sucess"){
                     dispatch_async(self.tictiQueue, { () -> Void in
                         callback(enviado: true);
